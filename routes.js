c7cce165354d3b59b51b7a561bfacd4edbd7236d
Tatu-Pekka Heikkilä <tatu-pekka.heikkila@tuni.fi>
2020-10-25 15:39:30 +0200

    8.3.3 return all users as json

--- routes.js
+++ routes.js
@@ -10,8 +10,8 @@ const { emailInUse, getAllUsers, saveNewUser, validateUser } = require('./utils/
  * in response to an OPTIONS request by sendOptions() (Access-Control-Allow-Methods)
  */
 const allowedMethods = {
-  '/api/register': ['POST'],
-  '/api/users': ['GET']
+    '/api/register': ['POST'],
+    '/api/users': ['GET']
 };
 
 /**
@@ -21,17 +21,17 @@ const allowedMethods = {
  * @param {http.ServerResponse} response
  */
 const sendOptions = (filePath, response) => {
-  if (filePath in allowedMethods) {
-    response.writeHead(204, {
-      'Access-Control-Allow-Methods': allowedMethods[filePath].join(','),
-      'Access-Control-Allow-Headers': 'Content-Type,Accept',
-      'Access-Control-Max-Age': '86400',
-      'Access-Control-Expose-Headers': 'Content-Type,Accept'
-    });
-    return response.end();
-  }
+    if (filePath in allowedMethods) {
+        response.writeHead(204, {
+            'Access-Control-Allow-Methods': allowedMethods[filePath].join(','),
+            'Access-Control-Allow-Headers': 'Content-Type,Accept',
+            'Access-Control-Max-Age': '86400',
+            'Access-Control-Expose-Headers': 'Content-Type,Accept'
+        });
+        return response.end();
+    }
 
-  return responseUtils.notFound(response);
+    return responseUtils.notFound(response);
 };
 
 /**
@@ -42,9 +42,9 @@ const sendOptions = (filePath, response) => {
  * @returns {boolean}
  */
 const matchIdRoute = (url, prefix) => {
-  const idPattern = '[0-9a-z]{8,24}';
-  const regex = new RegExp(`^(/api)?/${prefix}/${idPattern}$`);
-  return regex.test(url);
+    const idPattern = '[0-9a-z]{8,24}';
+    const regex = new RegExp(`^(/api)?/${prefix}/${idPattern}$`);
+    return regex.test(url);
 };
 
 /**
@@ -54,59 +54,61 @@ const matchIdRoute = (url, prefix) => {
  * @returns {boolean}
  */
 const matchUserId = url => {
-  return matchIdRoute(url, 'users');
+    return matchIdRoute(url, 'users');
 };
 
-const handleRequest = async (request, response) => {
-  const { url, method, headers } = request;
-  const filePath = new URL(url, `http://${headers.host}`).pathname;
+const handleRequest = async(request, response) => {
+    const { url, method, headers } = request;
+    const filePath = new URL(url, `http://${headers.host}`).pathname;
 
-  // serve static files from public/ and return immediately
-  if (method.toUpperCase() === 'GET' && !filePath.startsWith('/api')) {
-    const fileName = filePath === '/' || filePath === '' ? 'index.html' : filePath;
-    return renderPublic(fileName, response);
-  }
-
-  if (matchUserId(filePath)) {
-    // TODO: 8.5 Implement view, update and delete a single user by ID (GET, PUT, DELETE)
-    // You can use parseBodyJson(request) from utils/requestUtils.js to parse request body
-    throw new Error('Not Implemented');
-  }
-
-  // Default to 404 Not Found if unknown url
-  if (!(filePath in allowedMethods)) return responseUtils.notFound(response);
-
-  // See: http://restcookbook.com/HTTP%20Methods/options/
-  if (method.toUpperCase() === 'OPTIONS') return sendOptions(filePath, response);
-
-  // Check for allowable methods
-  if (!allowedMethods[filePath].includes(method.toUpperCase())) {
-    return responseUtils.methodNotAllowed(response);
-  }
-
-  // Require a correct accept header (require 'application/json' or '*/*')
-  if (!acceptsJson(request)) {
-    return responseUtils.contentTypeNotAcceptable(response);
-  }
-
-  // GET all users
-  if (filePath === '/api/users' && method.toUpperCase() === 'GET') {
-    // TODO: 8.3 Return all users as JSON
-    // TODO: 8.4 Add authentication (only allowed to users with role "admin")
-    throw new Error('Not Implemented');
-  }
-
-  // register new user
-  if (filePath === '/api/register' && method.toUpperCase() === 'POST') {
-    // Fail if not a JSON request
-    if (!isJson(request)) {
-      return responseUtils.badRequest(response, 'Invalid Content-Type. Expected application/json');
+    // serve static files from public/ and return immediately
+    if (method.toUpperCase() === 'GET' && !filePath.startsWith('/api')) {
+        const fileName = filePath === '/' || filePath === '' ? 'index.html' : filePath;
+        return renderPublic(fileName, response);
     }
 
-    // TODO: 8.3 Implement registration
-    // You can use parseBodyJson(request) from utils/requestUtils.js to parse request body
-    throw new Error('Not Implemented');
-  }
+    if (matchUserId(filePath)) {
+        // TODO: 8.5 Implement view, update and delete a single user by ID (GET, PUT, DELETE)
+        // You can use parseBodyJson(request) from utils/requestUtils.js to parse request body
+        throw new Error('Not Implemented');
+    }
+
+    // Default to 404 Not Found if unknown url
+    if (!(filePath in allowedMethods)) return responseUtils.notFound(response);
+
+    // See: http://restcookbook.com/HTTP%20Methods/options/
+    if (method.toUpperCase() === 'OPTIONS') return sendOptions(filePath, response);
+
+    // Check for allowable methods
+    if (!allowedMethods[filePath].includes(method.toUpperCase())) {
+        return responseUtils.methodNotAllowed(response);
+    }
+
+    // Require a correct accept header (require 'application/json' or '*/*')
+    if (!acceptsJson(request)) {
+        return responseUtils.contentTypeNotAcceptable(response);
+    }
+
+    // GET all users
+    if (filePath === '/api/users' && method.toUpperCase() === 'GET') {
+        let users = getAllUsers();
+        //console.log(users.type)
+        // TODO: 8.3 Return all users as JSON
+        // TODO: 8.4 Add authentication (only allowed to users with role "admin")
+        return responseUtils.sendJson(response, users);
+    }
+
+    // register new user
+    if (filePath === '/api/register' && method.toUpperCase() === 'POST') {
+        // Fail if not a JSON request
+        if (!isJson(request)) {
+            return responseUtils.badRequest(response, 'Invalid Content-Type. Expected application/json');
+        }
+
+        // TODO: 8.3 Implement registration
+        // You can use parseBodyJson(request) from utils/requestUtils.js to parse request body
+        throw new Error('Not Implemented');
+    }
 };
 
 module.exports = { handleRequest };
