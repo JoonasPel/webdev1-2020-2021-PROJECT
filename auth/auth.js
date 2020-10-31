8e8989075768cb3969b8af4608bd802883eb1fc7
Joonas <joonas.pelttari@tuni.fi>
2020-10-31 22:36:31 +0200

    auth.js ESLint fix

--- auth/auth.js
+++ auth/auth.js
@@ -7,19 +7,19 @@ const { getUser } = require('../utils/users');
  * @param {http.IncomingMessage} request
  * @returns {Object|null} current authenticated user or null if not yet authenticated
  */
-const getCurrentUser = async request => {
+const getCurrentUser = request => {
   // TODO: 8.4 Implement getting current user based on the "Authorization" request header
   // NOTE: You can use getCredentials(request) function from utils/requestUtils.js
   // and getUser(email, password) function from utils/users.js to get the currently
   // logged in user
 
   //get email and password or null if not authenticated
-  let userCredentials = getCredentials(request);
-  if(userCredentials == null ) {
+  const userCredentials = getCredentials(request);
+  if(userCredentials === null || userCredentials === undefined ) {
     return null;
   } else {
     //get user object
-    return getUser(userCredentials[0],userCredentials[1]);
+    return getUser(userCredentials[0], userCredentials[1]);
   }
 
 };
