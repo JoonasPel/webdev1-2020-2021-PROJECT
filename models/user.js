271ef6be25d1fdb8ffa06b58753fb21f2d9afd7e
Tatu-Pekka Heikkilä <tatu-pekka.heikkila@tuni.fi>
2020-11-04 10:49:21 +0200

    Detection for empty password fixed

--- models/user.js
+++ models/user.js
@@ -1,66 +1,70 @@
-const bcrypt = require('bcryptjs');
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+  const bcrypt = require('bcryptjs');
+  const mongoose = require('mongoose');
+  const Schema = mongoose.Schema;
 
-function encrypter(password) {
-  let hash = bcrypt.hashSync(password, 10);
-  return hash;
-}
-
-const userSchema = new Schema({
-  name: { 
-    type: String,
-    minlength: 1, 
-    maxlength: 50,
-    required: true,
-    trim: true
-  }, 
-  email: {
-    type: String,
-    required: true, 
-    unique: true, 
-    validate:
-     /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
-    },
-  password: { 
-    type: String, 
-    minlength : 10,
-    set: encrypter,
-    required: true
-  },
-  role: { 
-    type: String, 
-    required: true, 
-    enum: ['admin', 'customer'],
-    default: 'customer',
-    set: v => v.toLowerCase(),
-    trim: true
-  }
-});
-
-/**
- * Compare supplied password with user's own (hashed) password
- *
- * @param {string} password
- * @returns {Promise<boolean>} promise that resolves to the comparison result
- */
-userSchema.methods.checkPassword = async function (password) {
-
-  const isPasswordMatch = new Promise((resolve, reject) => {
-    bcrypt.compare(password, this.password, function(err, result) {
-      if(result === true) {
-        resolve(true);
-      } else {
-        resolve(false);
+  function encrypter(password) {
+      //console.log()
+      if (password === '') {
+          console.log('tyhjä')
+          return
       }
-    });  
+      let hash = bcrypt.hashSync(password, 10);
+      return hash;
+  }
+
+  const userSchema = new Schema({
+      name: {
+          type: String,
+          minlength: 1,
+          maxlength: 50,
+          required: true,
+          trim: true
+      },
+      email: {
+          type: String,
+          required: true,
+          unique: true,
+          validate: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+      },
+      password: {
+          type: String,
+          set: encrypter,
+          required: true
+
+      },
+      role: {
+          type: String,
+          required: true,
+          enum: ['admin', 'customer'],
+          default: 'customer',
+          set: v => v.toLowerCase(),
+          trim: true
+      }
   });
-  return isPasswordMatch;
-};
 
-// Omit the version key when serialized to JSON
-userSchema.set('toJSON', { virtuals: false, versionKey: false });
 
-const User = new mongoose.model('User', userSchema);
-module.exports = User;
+  /**
+   * Compare supplied password with user's own (hashed) password
+   *
+   * @param {string} password
+   * @returns {Promise<boolean>} promise that resolves to the comparison result
+   */
+  userSchema.methods.checkPassword = async function(password) {
 
+      const isPasswordMatch = new Promise((resolve, reject) => {
+          bcrypt.compare(password, this.password, function(err, result) {
+              if (result === true) {
+                  resolve(true);
+              } else {
+                  resolve(false);
+              }
+          });
+      });
+      return isPasswordMatch;
+  };
+
+  // Omit the version key when serialized to JSON
+  userSchema.set('toJSON', { virtuals: false, versionKey: false });
+
+  const User = new mongoose.model('User', userSchema);
+  module.exports = User;
