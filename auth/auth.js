f451ffcff475e7bd765f481b936152992884febf
Tatu-Pekka Heikkilä <tatu-pekka.heikkila@tuni.fi>
2020-11-10 14:04:10 +0200

    Minor fixes

--- auth/auth.js
+++ auth/auth.js
@@ -1,5 +1,4 @@
 const { getCredentials } = require('../utils/requestUtils');
-const { getUser } = require('../utils/users');
 const mongoose = require('mongoose');
 const User = require('../models/user');
 
@@ -10,36 +9,38 @@ const User = require('../models/user');
  * @returns {Object|null} current authenticated user or null if not yet authenticated
  */
 const getCurrentUser = async request => {
-  // TODO: 8.4 Implement getting current user based on the "Authorization" request header
-  // NOTE: You can use getCredentials(request) function from utils/requestUtils.js
-  // and getUser(email, password) function from utils/users.js to get the currently
-  // logged in user
+    // TODO: 8.4 Implement getting current user based on the "Authorization" request header
+    // NOTE: You can use getCredentials(request) function from utils/requestUtils.js
+    // and getUser(email, password) function from utils/users.js to get the currently
+    // logged in user
 
-  //get email and password or null if not authenticated
-  const userCredentials = getCredentials(request);
-  if (userCredentials === null || userCredentials === undefined) {
-    return null;
-  } else {
-    //get user object
-    //return getUser(userCredentials[0], userCredentials[1]);
-    const givenEmail = userCredentials[0];
-    const givenPassword = userCredentials[1];
-    const currentUser = await User.findOne({ email: givenEmail });
-    //checks if user is not found from database
-    if(currentUser === null || currentUser === undefined) {
-      return null;
-    }
-    //check if password matches
-    if (await currentUser.checkPassword(givenPassword)) {
-      // password matched
-      return currentUser;
+    //get email and password or null if not authenticated
+    const userCredentials = getCredentials(request);
+    if (userCredentials === null || userCredentials === undefined) {
+        return null;
     } else {
-      // password did not match
-      return null;
-    }
+        //get user object
+        //return getUser(userCredentials[0], userCredentials[1]);
+        const givenEmail = userCredentials[0];
+        const givenPassword = userCredentials[1];
+        const currentUser = await User.findOne({ email: givenEmail });
+        //checks if user is not found from database
+        if (currentUser === null || currentUser === undefined) {
+            return null;
+        }
+        //check if password matches
+        if (await currentUser.checkPassword(givenPassword)) {
+            // password matched
+            return currentUser;
+        } else {
+            // password did not match
+            return null;
+        }
 
-  }
+    }
 
 };
+
+
 
 module.exports = { getCurrentUser };
