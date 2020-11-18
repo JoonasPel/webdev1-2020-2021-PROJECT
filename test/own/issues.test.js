5bf8e5fa90dad5c2656ffd3eec53d6f118b0ccf5
Tatu-Pekka Heikkilä <tatu-pekka.heikkila@tuni.fi>
2020-11-18 19:24:55 +0200

    Own issues testing

---
+++ test/own/issues.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+describe('UI: Show cart items count in UI', () => {
+    it('cart should show count increasing when adding items to cart', async() => {
+        expect(count).to.be(0);
+    });
+});
+describe('Allow user to change their email', () => {
+    it('User should see updated email', async() => {
+        const email = 'test@mail.com'
+        expect(newEmail).to.be(email);
+    });
+});
+describe('Allow user to change their name', () => {
+    it('User should see updated name', async() => {
+        const name = 'test@mail.com'
+        expect(newName).to.be(name);
+    });
+});
+describe('Allow user to change their password', () => {
+    it('User should be able to change their password', async() => {
+        const password = 'test@mail.com'
+        expect(newPassword).to.be(password);
+    });
+});
+describe('Implement DB connection to products', () => {
+    it('Products should be in database', async() => {
+        expect(thisfail);
+    });
+});
