f61c79c999c794eb895bdf5cb95658b21264d68a
Joonas Pelttari <joonas.pelttari@tuni.fi>
2020-11-25 17:38:38 +0200

    Revert "own tests always passing"
    This reverts commit ef059fd6f6da915614b782fff4c4139270afa013.

--- test/own/issues.test.js
+++ test/own/issues.test.js
@@ -3,61 +3,51 @@ const { errors } = require('puppeteer');
 const expect = chai.expect;
 describe('UI: Show cart items count in UI', () => {
     it('cart should show count increasing when adding items to cart', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Allow user to change their email', () => {
     it('User should see updated email', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Allow user to change their name', () => {
     it('User should see updated name', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Allow user to change their password', () => {
     it('User should be able to change their password', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Implement DB connection to products', () => {
     it('Products should be in database', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Allow user to save their shopping cart', () => {
     it('User should be able to save shopping cart', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Show the total price of products in a shopping cart', () => {
     it('Products in shopping cart total sum should be correct', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Allow user to reset his/her password via email', () => {
     it('User should get new email with request', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Get email-notification when product is available', () => {
     it('User should get email notification when product is in stock', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
 describe('Fix Eslint errors', () => {
     it('Eslint should not show errors', async() => {
-        //throw new Error();
-        expect(1).to.equal(1);
+        throw new Error();
     });
 });
